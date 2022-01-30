9b1e20baeea08162d070fb8d3103bb34059eba06
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-30 14:17:59 -0300

    c1- create new course about learning docker

--- api/db/script.sql
+++ api/db/script.sql
@@ -2,12 +2,14 @@ CREATE TABLE IF NOT EXIST
 docker;
 USE docker;
 
-CREATE TABLE IF NOT EXIST products(
+CREATE TABLE products(
     id INT(11) AUTO_INCREMENT,
     name VARCHAR(255),
     price DECIMAL(10,2),
     PRIMARY KEY(id)
     );
 
-INSERT INTO products VALUE(120, 'front-end', 1000);
-INSERT INTO products VALUE(99, 'Back-end', 60);
+INSERT INTO products VALUE(120, 'course-frontend', 1000);
+INSERT INTO products VALUE(99, 'course-backend', 60);
+INSERT INTO products VALUE(300, 'course about features about api REST',100);
+INSERT INTO products VALUE(1, 'course learning create docker file',150)
